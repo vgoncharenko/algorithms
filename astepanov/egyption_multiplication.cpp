745a1ecd796fccf15b90fe915fc5d14d79a5fbab
Vitaliy Honcharenko <vgoncharenko@magento.com>
2020-09-24 19:38:23 -0500

    egpt_mult

--- astepanov/egyption_multiplication.cpp
+++ astepanov/egyption_multiplication.cpp
@@ -7,7 +7,7 @@
  * Naive approach - SUM. O(y)
  */
 template<typename T>
-T egpt_mult1(T x, T y) {
+T mult1(T x, T y) {
   T result = 0;
   for (T i=0; i<y; i++) {
     result += x;
@@ -19,7 +19,7 @@ T egpt_mult1(T x, T y) {
  * Naive approach - SUM. O(min(x,y))
  */
 template<typename T>
-T egpt_mult2(T x, T y) {
+T mult2(T x, T y) {
   T min_v = min(x, y);
   T max_v = max(x, y);
   T result = max_v;
@@ -30,10 +30,10 @@ T egpt_mult2(T x, T y) {
 }
 
 /*
- * Egyptian multiplication (recursive) O(log(n))
+ * Egyptian multiplication (recursive) O(log(min(x,y))+3)
  *
  * a = 4;
- * b = 3;
+ * b = 4;
  *
  * Law of associativity:
  * result = a + a + a + a = (a + a) + (a + a)
@@ -60,7 +60,7 @@ T egpt_mult3_helper(T x, T n) {
 }
 
 template<typename T>
-T egpt_mult3(T x, T y) {
+T mult3(T x, T y) {
   T min_v = min(x, y);
   T max_v = max(x, y);
 
@@ -68,7 +68,7 @@ T egpt_mult3(T x, T y) {
 }
 
 /**
- * Egyptian multiplication (recursive) +small optimization for if (n == 1) branch O(log(n))
+ * Egyptian multiplication (recursive) +small optimization for if (n == 1) branch O(log(min(x,y)))
  */
 template<typename T>
 T egpt_mult4_helper(T r, T x, T n) {
@@ -81,7 +81,7 @@ T egpt_mult4_helper(T r, T x, T n) {
 }
 
 template<typename T>
-T egpt_mult4(T x, T y) {
+T mult4(T x, T y) {
   T min_v = min(x, y);
   T max_v = max(x, y);
 
@@ -90,7 +90,7 @@ T egpt_mult4(T x, T y) {
 
 
 /**
- * Egyptian multiplication No Recursion O(log(n)).
+ * Egyptian multiplication No Recursion O(log(min(x,y))).
  */
 template<typename T>
 T egpt_mult5_helper(T r, T x, T n) {
@@ -106,7 +106,7 @@ T egpt_mult5_helper(T r, T x, T n) {
 }
 
 template<typename T>
-T egpt_mult5(T x, T y) {
+T mult5(T x, T y) {
   T min_v = min(x, y);
   T max_v = max(x, y);
 
@@ -114,7 +114,7 @@ T egpt_mult5(T x, T y) {
 }
 
 /**
- * Egyptian multiplication. Optimization for even number of multiplication O(log(n)).
+ * Egyptian multiplication. Optimization for even number of multiplication O(log(min(x,y))).
  */
 template<typename T>
 T egpt_mult6_helper(T r, T x, T n) {
@@ -130,7 +130,7 @@ T egpt_mult6_helper(T r, T x, T n) {
 }
 
 template<typename T>
-T egpt_mult6(T x, T y) {
+T mult6(T x, T y) {
   T min_v = min(x, y);
   T max_v = max(x, y);
 
@@ -147,7 +147,7 @@ T egpt_mult6(T x, T y) {
  * Processor's multiplication O(1)
  */
 template<typename T>
-T egpt_mult7(T x, T y) {
+T mult7(T x, T y) {
   return x * y;
 }
 
@@ -156,50 +156,50 @@ void testEgyptMult() {
 //  long y = 98459903;
 //  long expected = 969399085554569;
 
-//  long x = 8388608;
-//  long y = 67108865;
-//  long expected = 562949961809920;
+  long x = 8388608;
+  long y = 67108865;
+  long expected = 562949961809920;
 
 //  long x = 99;
 //  long y = 100;
 //  long expected = 9900;
 
-  long x = 8388609;
-  long y = 67108865;
-  long expected = 562950028918785;
+//  long x = 8388609;
+//  long y = 67108865;
+//  long expected = 562950028918785;
   long result = 0;
 
   std::cout << "Naive approach - SUM. O(y):" << std::endl;
-  measure( [&x, &y, &result]{ result = egpt_mult1(x, y); } );
+  measure( [&x, &y, &result]{ result = mult1(x, y); } );
   assert(expected == result);
 
   std::cout << "\n\nNaive approach - SUM. O(min(x,y)):" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult2(x, y); } );
+  measure( [&x, &y, &result]{ result = mult2(x, y); } );
   assert(expected == result);
 
-  std::cout << "\n\nEgyptian multiplication (recursive) O(log(n)):" << std::endl;
+  std::cout << "\n\nEgyptian multiplication (recursive) O(log(min(x,y))):" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult3(x, y); } );
+  measure( [&x, &y, &result]{ result = mult3(x, y); } );
   assert(expected == result);
 
-  std::cout << "\n\nEgyptian multiplication (recursive) +small optimization for if (n == 1) branch O(log(n)):" << std::endl;
+  std::cout << "\n\nEgyptian multiplication (recursive) +small optimization for if (n == 1) branch O(log(min(x,y))):" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult4(x, y); } );
+  measure( [&x, &y, &result]{ result = mult4(x, y); } );
   assert(expected == result);
 
-  std::cout << "\n\nEgyptian multiplication No Recursion O(log(n)):" << std::endl;
+  std::cout << "\n\nEgyptian multiplication No Recursion O(log(min(x,y))):" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult5(x, y); } );
+  measure( [&x, &y, &result]{ result = mult5(x, y); } );
   assert(expected == result);
 
-  std::cout << "\n\nEgyptian multiplication. Optimization for even number of multiplication O(log(n)):" << std::endl;
+  std::cout << "\n\nEgyptian multiplication. Optimization for even number of multiplication O(log(min(x,y))):" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult6(x, y); } );
+  measure( [&x, &y, &result]{ result = mult6(x, y); } );
   assert(expected == result);
 
   std::cout << "\n\nProcessor's multiplication O(1)" << std::endl;
   result = 0;
-  measure( [&x, &y, &result]{ result = egpt_mult7(x, y); } );
+  measure( [&x, &y, &result]{ result = mult7(x, y); } );
   assert(expected == result);
 }

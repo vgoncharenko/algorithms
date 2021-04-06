5a38f2c7193a71d5a9f563601ebc0cd29349eb8e
Vitaliy Honcharenko <vgoncharenko@magento.com>
2021-04-06 15:00:28 -0500

    MPI M x V Parallel Striped horizontal

--- graphic/MatrixVectorMult.cpp
+++ graphic/MatrixVectorMult.cpp
@@ -74,6 +74,102 @@ void testMatrixVectorMultParallelStriped(float* matrix,
     });
 }
 
+// Data distribution among the processes
+void DataDistribution(const float* pMatrix,
+                      float* pProcRows,
+                      float* pVector,
+                      const uint64_t Size,
+                      int RowNum,
+                      int ProcNum,
+                      int ProcRank) {
+    int *pSendNum; // the number of elements sent to the process
+    int *pSendInd; // the index of the first data element sent to the process
+    int RestRows=Size; // Number of rows, that haven’t been distributed yet
+
+    MPI_Bcast(pVector, Size, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+// Alloc memory for temporary objects
+    pSendInd = new int [ProcNum];
+    pSendNum = new int [ProcNum];
+// Define the disposition of the matrix rows for current process
+    RowNum = (Size/ProcNum);
+    pSendNum[0] = RowNum*Size;
+    pSendInd[0] = 0;
+    for (int i=1; i<ProcNum; i++) {
+        RestRows -= RowNum;
+        RowNum = RestRows/(ProcNum-i);
+        pSendNum[i] = RowNum*Size;
+        pSendInd[i] = pSendInd[i-1]+pSendNum[i-1];
+    }
+// Scatter the rows
+    MPI_Scatterv(pMatrix , pSendNum, pSendInd, MPI_FLOAT, pProcRows,
+                 pSendNum[ProcRank], MPI_FLOAT, 0, MPI_COMM_WORLD);
+// Free the memory
+    delete [] pSendNum;
+    delete [] pSendInd;
+}
+
+void ParallelResultCalculation(const float* pProcRows,
+                               const float* pVector,
+                               float* pProcResult,
+                               const uint64_t Size,
+                               int RowNum) {
+    for (int i=0; i<RowNum; i++) {
+        pProcResult[i] = 0;
+        for (int j=0; j<Size; j++)
+            pProcResult[i] += pProcRows[i*Size+j]*pVector[j];
+    }
+}
+
+// Function for gathering the result vector
+void ResultReplication(float* pProcResult,
+                       float* pResult,
+                       const uint64_t Size,
+                       int ProcNum,
+                       int ProcRank) {
+    int i; // Loop variable
+    int *pReceiveNum; // Number of elements, that current process sends
+    int *pReceiveInd; /* Index of the first element from current process
+in result vector */
+    int RestRows=Size; // Number of rows, that haven’t been distributed yet
+//Alloc memory for temporary objects
+    pReceiveNum = new int [ProcNum];
+    pReceiveInd = new int [ProcNum];
+//Define the disposition of the result vector block of current processor
+    pReceiveInd[0] = 0;
+    pReceiveNum[0] = Size/ProcNum;
+    for (i=1; i<ProcNum; i++) {
+        RestRows -= pReceiveNum[i-1];
+        pReceiveNum[i] = RestRows/(ProcNum-i);
+        pReceiveInd[i] = pReceiveInd[i-1]+pReceiveNum[i-1];
+    }
+//Gather the whole result vector on every processor
+    MPI_Allgatherv(pProcResult, pReceiveNum[ProcRank], MPI_DOUBLE, pResult,
+                   pReceiveNum, pReceiveInd, MPI_DOUBLE, MPI_COMM_WORLD);
+//Free the memory
+    delete [] pReceiveNum;
+    delete [] pReceiveInd;
+}
+
+void testMatrixVectorMultMPIParallelHorizontalStriped(const float* matrix,
+                                                      float* vIn,
+                                                      float* vOut,
+                                                      const uint64_t n,
+                                                      int ProcNum) {
+    int *argc;
+    char ***argv;
+    int ProcRank = 1;
+    float* pProcRows;
+    float* pProcResult;
+    int RowNum;
+    MPI_Init(argc, argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
+    MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
+    DataDistribution(matrix, pProcRows, vIn, n, RowNum, ProcNum, ProcRank);
+    ParallelResultCalculation(pProcRows, vIn, pProcResult, n, RowNum);
+    ResultReplication(pProcResult, vOut, n, ProcNum, ProcRank);
+    MPI_Finalize();
+}
+
 void verifyVectors(const float* expected, const float* actual, const uint64_t n) {
     for (int i = 0; i < n; ++i) {
         if (expected[i] != actual[i])
